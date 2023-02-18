1856cd0ea9799c09b5972f332b87493e79f2620a
fruits <fruits3>
2023-02-17 18:09:27 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	lastd = n % 10;
 	if (lastd > 5)
 	{
-	pintf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
-	else if (lasted == 0)
+	else if (lastd == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, lasted);
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
-	else if (lasted < 6 && lastd != 0)
+	else if (lastd < 6 && lastd != 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lasted);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }

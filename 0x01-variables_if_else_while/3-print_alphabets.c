768f6f656aafd94d3cb36f9ad0ebf0ef61d59382
fruits <fruits3>
2023-02-17 18:51:44 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 
 }
-
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
@@ -22,6 +18,5 @@ int main(void)
 	putchar(low);
 	putchar('\n');
 
-	return(0);
-
+	return (0);
 }

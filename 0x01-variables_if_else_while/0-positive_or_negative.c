1fd195e795f309dcfbc00ecfc965ba6adbc431ec
fruits <fruits3>
2023-02-17 17:32:01 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-
 /**
- * main - Prints if number is positive, zero or negative
- * Return: Always (success)
- *
- */
+* main - Prints if number is positive, zero or negative
+* Return: Always (success)
+*
+*/
 
 int main(void)
-
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
